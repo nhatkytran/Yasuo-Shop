a4b7058505437f0fad0500bccba232efe07e139e
nhatkytran <nhatky.tran.2002@gmail.com>
2024-04-26 15:58:28 +0700

    Server - Handle errors: Unhandled rejections, uncaught exceptions and SIGTERM

--- server/src/connections/database.ts
+++ server/src/connections/database.ts
@@ -1,18 +1,14 @@
-const mongoose = require("mongoose");
-import config from "config";
+const mongoose = require('mongoose');
+import config from 'config';
 
-import logger from "../utils/logger";
+import logger from '../utils/logger';
 
 const connectDatabase = async () => {
-  try {
-    await mongoose
-      .set("strictQuery", true)
-      .connect(config.get<string>("databaseURL"));
+  await mongoose
+    .set('strictQuery', true)
+    .connect(config.get<string>('databaseURL'));
 
-    logger.info("Database connection - Successful!");
-  } catch (error) {
-    logger.error(error, "Database connection - Failed!");
-  }
+  logger.info('Database connection - Successful!');
 };
 
 export default connectDatabase;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,11 @@
+import logger from './utils/logger';
+
+// All errors happening in synchronous code -> nodejs process is in unclean state
+process.on('uncaughtException', error => {
+  logger.error(error, '--- UNCAUGHT EXCEPTION! Shutting down... ---');
+  process.exit(1);
+});
+
 import { Express } from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
@@ -6,16 +14,29 @@ dotenv.config({ path: path.join(__dirname, '../.env') });
 
 import config from 'config';
 import init from './app';
-import logger from './utils/logger';
 import connectDatabase from './connections/database';
 
 const app: Express = init();
 
 const port = config.get<number>('port') || 1337;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   logger.info(`App is running at port ${port}...`);
 
   // Connect MongoDB Cloud - Atlas
   connectDatabase();
+});
+
+// There is a promise that got rejected but it has not been handled
+process.on('unhandledRejection', error => {
+  logger.error(error, '--- UNHANDLED REJECTION! Shutting down... ---');
+  // number 0 -> exit after success
+  // number 1 -> some kind of error and give more information about it
+  server.close(() => process.exit(1));
+});
+
+// The NodeJS process is killed
+process.on('SIGTERM', () => {
+  logger.info('--- SIGTERM RECEIVED! Shutting down... ---');
+  if (server) server.close(() => logger.info('SIGTERM - Process terminated!'));
 });
